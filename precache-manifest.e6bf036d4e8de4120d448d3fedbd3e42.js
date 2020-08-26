753646f18a42611b70b5a551dbc0cacbbe9e20ea
John Elesho <eleshojohno@gmail.com>
2020-08-26 18:04:21 +0100

    Updates

---
+++ precache-manifest.e6bf036d4e8de4120d448d3fedbd3e42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230f23e1f85430dcfca581a084888117",
+    "url": "/react-comments-pagination/index.html"
+  },
+  {
+    "revision": "ff60cf01b345c0ad89ca",
+    "url": "/react-comments-pagination/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "fee8bff5425b9105f8a5",
+    "url": "/react-comments-pagination/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ff60cf01b345c0ad89ca",
+    "url": "/react-comments-pagination/static/js/2.e47551da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-comments-pagination/static/js/2.e47551da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee8bff5425b9105f8a5",
+    "url": "/react-comments-pagination/static/js/main.1f9af03a.chunk.js"
+  },
+  {
+    "revision": "53c5e97d5e5651e950ee",
+    "url": "/react-comments-pagination/static/js/runtime-main.ed09c748.js"
+  }
+]);
